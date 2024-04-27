1647596c7ed3bca199188eb8043f8ac0deb63958
mohit7479 <mohit7479936440@gmail.com>
2024-04-28 01:37:44 +0530

    Create connection with database

--- ChatApp-main/backend/server.js
+++ ChatApp-main/backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const chats = require("./data/data.js");
-const env = require("dotenv");
+const dotenv = require("dotenv");
 const cors = require("cors"); // Import cors package
+const connectDB = require("./config/db.js");
+const color = require('colors');
 
 const app = express();
-env.config();
+dotenv.config();
+connectDB();
 
 // Enable CORS for all routes
 app.use(cors());
@@ -20,5 +23,5 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server started on Port ${port}`);
+  console.log(`Server started on Port ${port}`.yellow.bold);
 });
